3acb1695f756e7a221d57f57a75c45109cbb2d0a
Matheus Bessa <mdabessa@gmail.com>
2022-09-07 18:03:17 -0300

    async loop

--- scraping/animes/loop.py
+++ scraping/animes/loop.py
@@ -1,7 +1,12 @@
+import asyncio
 from main import main
-from time import sleep
 
 
-while True:
-    main()
-    sleep(600)
+async def main():
+    while True:
+        await main()
+        await asyncio.sleep(600)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

77b7900953b2803f327866975854e5f82e1f306a
Matheus Bessa <mdabessa@gmail.com>
2022-09-11 23:50:02 -0300

    Update loop.py

--- scraping/animes/loop.py
+++ scraping/animes/loop.py
@@ -2,11 +2,11 @@ import asyncio
 from main import main
 
 
-async def main():
+async def loop():
     while True:
         await main()
         await asyncio.sleep(600)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(loop())

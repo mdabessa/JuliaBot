e974599fa118a4f440860e46e39f4c6e89dca29a
Matheus Bessa <mdabessa@gmail.com>
2024-12-22 23:40:29 -0300

    Fix month and year precision

--- tests/test_date_converter.py
+++ tests/test_date_converter.py
@@ -28,7 +28,46 @@ async def test_date_converter():
 
 @pytest.mark.asyncio
 async def test_delta_to_date_converter():
-    ...
+    tests = [
+        [
+            datetime.datetime(2024, 10, 24, hour=17, minute=6),
+            datetime.datetime(2024, 10, 25, hour=17, minute=6),
+            "1d"
+        ],
+
+        [
+            datetime.datetime(2024, 10, 24, hour=17, minute=6),
+            datetime.datetime(2024, 10, 24, hour=17, minute=16),
+            "10m"
+        ],
+
+        [
+            datetime.datetime(2024, 10, 24, hour=17, minute=6),
+            datetime.datetime(2024, 10, 24, hour=18, minute=6),
+            "1h"
+        ],
+
+        [
+            datetime.datetime(2024, 10, 24, hour=17, minute=6),
+            datetime.datetime(2024, 10, 24, hour=18, minute=16),
+            "1h10m"
+        ],
+
+
+        [
+            datetime.datetime(2024, 10, 24, hour=17, minute=6),
+            datetime.datetime(2026, 1, 26, hour=19, minute=16),
+            "1y3mes2d2h10m"
+        ],
+    ]
+
+    for test in tests:
+        date, expected, delta = test
+        dateConverted = await DeltaToDate().convert(None, delta, start=date)
+        
+        assert expected == dateConverted
+
+
 
 @pytest.mark.asyncio
 async def test_next_date_converter():

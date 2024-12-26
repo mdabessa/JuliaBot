a7954aed564c0dd91a7120753014a4c3bb0350c7
Matheus Bessa <mdabessa@gmail.com>
2024-12-26 16:24:44 -0300

    Fix NextDate converter logic to handle edge case for start time and update tests

--- juliabot/converters.py
+++ juliabot/converters.py
@@ -146,7 +146,7 @@ class NextDate(commands.Converter):
         for step, (num, _) in times.items():
             date += relativedelta(**{step: num})
 
-            if date < start:
+            if ((date < start) or ((date == start) and (step == "minute"))):
                 index = list(STEPS.keys()).index(step)
 
                 while True:
--- tests/test_date_converter.py
+++ tests/test_date_converter.py
@@ -95,6 +95,11 @@ async def test_next_date_converter():
             datetime.datetime(2024, 12, 20, hour=13, minute=16),
             "m16",
         ],
+                [
+            datetime.datetime(2024, 12, 26, hour=8, minute=0),
+            datetime.datetime(2024, 12, 27, hour=8, minute=0),
+            "h8m00",
+        ],
     ]
 
     for test in tests:
